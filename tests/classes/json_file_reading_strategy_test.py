86297a9aad78adaf778c845a25a6ad8329df03c2
Cl1nical <cl1nical@yahoo.com>
2026-02-18 13:03:48 +0100

    updated a test method name

--- tests/classes/json_file_reading_strategy_test.py
+++ tests/classes/json_file_reading_strategy_test.py
@@ -3,11 +3,10 @@ from pathlib import Path
 from src.interfaces.file_reading_strategy import FileReadingStrategy
 from src.classes.json_file_reading_strategy import JsonFileReadingStrategy
 
-# Check that something test
-def test_FileReadingStrategy_read():
+
+def test_FileReadingStrategy_read_method_returns_json_object():
     test_file = Path(__file__).parent.parent.parent / "files" / "test_files" / "test_file0.json"
     fileStrategy: FileReadingStrategy = JsonFileReadingStrategy()
     tmp = fileStrategy.read(test_file)
-    print("tmp:", tmp)
     result = tmp[0].get("component")
     assert "physical_twin" == result
