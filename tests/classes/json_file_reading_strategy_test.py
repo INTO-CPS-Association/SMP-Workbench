0cad5dab91eba4a236ddf11f4d6f37754fb4e2ca
Cl1nical <cl1nical@yahoo.com>
2026-02-18 12:51:47 +0100

    Test push

--- tests/classes/json_file_reading_strategy_test.py
+++ tests/classes/json_file_reading_strategy_test.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from src.interfaces.file_reading_strategy import FileReadingStrategy
 from src.classes.json_file_reading_strategy import JsonFileReadingStrategy
 
+# Check that something test
 def test_FileReadingStrategy_read():
     test_file = Path(__file__).parent.parent.parent / "files" / "test_files" / "test_file0.json"
     fileStrategy: FileReadingStrategy = JsonFileReadingStrategy()
