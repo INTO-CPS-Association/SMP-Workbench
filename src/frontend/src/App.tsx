c430a460b1d61d5a56418cf9c748793ab93b85e0
Cl1nical <cl1nical@yahoo.com>
2026-04-01 08:12:31 +0200

    Minor changes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -5,11 +5,15 @@ import FruitList from './components/Fruits';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Fruit Management App</h1>
+      <header className="App- header">
+        <h1>SMP-Visualizer</h1>
       </header>
+      
       <main>
-        <FruitList />
+        <h3>Select files to analyze</h3>  
+        <label>
+            <input type="file" id="file-picker" name="fileList" webkitdirectory multiple />
+        </label>
       </main>
     </div>
   );
